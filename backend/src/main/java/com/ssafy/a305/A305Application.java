5831ccda3b3fa99ac8af06e9b71c19e402192e30
khs <anjfkrhdkdle@naver.com>
2023-07-20 12:29:36 +0900

    initial commit

---
+++ backend/src/main/java/com/ssafy/a305/A305Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.a305;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A305Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A305Application.class, args);
+	}
+
+}
