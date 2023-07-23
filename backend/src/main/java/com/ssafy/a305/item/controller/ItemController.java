2cd38a15a51bc8ab25dfd0ea2e15d1a56ac40f0e
khs960616 <anjfkrhdkdle@naver.com>
2023-07-24 00:47:21 +0900

    feat: 아이템 목록 조회 API 추가

---
+++ backend/src/main/java/com/ssafy/a305/item/controller/ItemController.java
@@ -0,0 +1,22 @@
+package com.ssafy.a305.item.controller;
+
+import com.ssafy.a305.item.dto.ItemsReqDTO;
+import com.ssafy.a305.item.dto.ItemsResDTO;
+import com.ssafy.a305.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items")
+    public ResponseEntity<ItemsResDTO> getItems(@RequestParam ItemsReqDTO dto) {
+        return ResponseEntity.ok(itemService.getItems(dto));
+    }
+}
