caf1955bc9117aef7538f370d9fa0a84387667de
고서영 <seosue0801@gmail.com>
2023-07-24 15:52:54 +0900

    Feature/navbar

---
+++ frontend/src/components/NavBar/LogIn.tsx
@@ -0,0 +1,11 @@
+import styles from './NavBar.module.css';
+
+const LogIn = () => {
+    return (
+        <div className={styles['login']}>
+            로그인
+        </div>
+    )
+}
+
+export default LogIn;
