3401161fd30d5c394e367313826ce3d5d5bd69d3
김한성 <anjfkrhdkdle@naver.com>
2023-08-03 12:31:59 +0900

    Feature/stage-selector

---
+++ frontend/src/web/pages/GameTestPage.tsx
@@ -0,0 +1,10 @@
+import { createGame } from "../../game";
+
+const GameTestPage = () => {
+    createGame(960, 600, "game");
+    return (
+        <div id="game"></div>
+    )
+}
+
+export default GameTestPage;
