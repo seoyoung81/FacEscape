af0716eabf835ced6d7ee0afcbb77d3dbdad98b7
asusikai <adj0707@gmail.com>
2023-08-04 13:26:07 +0900

    map 업데이트

--- frontend/src/game/scene/stage01.ts
+++ frontend/src/game/scene/stage01.ts
@@ -2,12 +2,6 @@ import * as Phaser from "phaser";
 import map from "../assets/data/stage01.json";
 import background from "../assets/images/background.png";
 import terrain from "../assets/images/Terrain.png";
-
-//====== wall setting ==============
-const WALL_START_X = 100;
-const WALL_START_Y = 510;
-const WALL_GAP = 55;
-const WALL_Y_OFFSET = 5; // Positioned slightly above the wall below it
 export default class Stage01 extends Phaser.Scene {
   constructor() {
     super({
@@ -17,13 +11,13 @@ export default class Stage01 extends Phaser.Scene {
   }
 
   preload(): void {
-    this.load.tilemapTiledJSON("map", map);
     this.load.image("bg", background);
+    this.load.tilemapTiledJSON("map", map);
     this.load.image("terrain", terrain);
   }
 
   create(): void {
-    this.add.image(480, 360, "bg");
+    this.add.image(480, 300, "bg");
     const map = this.make.tilemap({
       key: "map",
       tileWidth: 16,
--- frontend/src/game/scene/stage02.ts
+++ frontend/src/game/scene/stage02.ts
@@ -1,7 +1,7 @@
 import Phaser from "phaser";
 
 import map from "../assets/data/stage02.json";
-
+import terrain from "../assets/images/Terrain.png";
 export default class Stage02 extends Phaser.Scene {
   constructor() {
     super({
@@ -9,13 +9,14 @@ export default class Stage02 extends Phaser.Scene {
     });
   }
 
-  mapWidth: number = 20;
-  mapHeight: number = 55;
+  mapWidth: number = 60;
+  mapHeight: number = 100;
   tileWidth: number = 16;
   tileHeight: number = 16;
 
   preload(): void {
     this.load.tilemapTiledJSON("map", map);
+    this.load.image("terrain", terrain);
   }
 
   create(): void {
@@ -23,7 +24,10 @@ export default class Stage02 extends Phaser.Scene {
       0,
       0,
       this.mapWidth * this.tileWidth,
-      this.mapHeight * this.tileHeight
+      this.mapHeight * this.tileHeight,
+
+      // centerize
+      true
     );
     this.physics.world.setBounds(
       0,
@@ -31,6 +35,18 @@ export default class Stage02 extends Phaser.Scene {
       this.mapWidth * this.tileWidth,
       this.mapHeight * this.tileHeight
     );
+
+    this.cameras.main.scrollX = 100;
+    this.cameras.main.scrollY = 1300;
+    this.cameras.main.setZoom(1);
+
+    const map = this.make.tilemap({
+      key: "map",
+      tileWidth: 16,
+      tileHeight: 16,
+    });
+    map.addTilesetImage("terrain", "terrain");
+    map.createLayer("platformLayer", ["terrain"]);
   }
 
   update(): void {}
