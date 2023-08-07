5b547331a2efadb5068c0a4d3ecfe2cbdb8fb16b
seojoonhee <seojoonhee10@gmail.com>
2023-08-07 14:15:18 +0900

    feat:stage2,3 구성요소 객체 추가

---
+++ frontend/src/game/object/trafficlight.ts
@@ -0,0 +1,63 @@
+import * as Phaser from "phaser";
+
+export class TrafficLight extends Phaser.GameObjects.Sprite {
+
+  private trafficLightState: string = 'green';
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    texture: string,
+  ) {
+    super(scene, x, y, texture);
+
+    this.scene = scene;
+    this.scene.add.existing(this);
+
+    // 신호등 시간 설정 (/1000 = 초)
+    const GREEN_DELAY = 5000;
+    const YELLOW_DELAY = 1000;
+    const RED_DELAY = 2000;
+
+    // 애니메이션 설정
+    const trafficLightFrames = this.anims.generateFrameNumbers('trafficLight', { start: 0, end: 2 });
+    const trafficLightFrameConfig = {
+        key: 'trafficLight_anim',
+        frames: trafficLightFrames,
+        frameRate: 1,
+        repeat: -1
+    };
+
+    // 애니메이션 생성
+    const delays = [GREEN_DELAY, YELLOW_DELAY, RED_DELAY];
+    this.setFrameDelays(trafficLightFrameConfig, delays);
+    this.anims.create(trafficLightFrameConfig);
+    this.play("trafficLight_anim");
+
+    this.on('animationupdate', () => {
+        const { index } = this.anims.currentFrame as Phaser.Animations.AnimationFrame;
+        switch (index) {
+        case 1:
+            this.trafficLightState = 'green';
+            break;
+        case 2:
+            this.trafficLightState = 'yellow';
+            break;
+        default:
+            this.trafficLightState = 'red';
+        }
+    }, this);
+
+  }
+  
+  setFrameDelays(animationConfig: any, delays: any) {
+    for (let i = 0; i < animationConfig.frames.length; i++) {
+        animationConfig.frames[i].duration = delays[i];
+        }
+  }
+
+  update(): void {
+    console.log(this.trafficLightState);
+  }
+}
--- frontend/src/game/scene/stage02.ts
+++ frontend/src/game/scene/stage02.ts
@@ -1,7 +1,12 @@
 import Phaser from "phaser";
-import { Player } from "../object/player";
 import map from "../assets/data/stage02.json";
 import terrain from "../assets/images/terrain.png";
+import timeGaugePNG from "../assets/images/timegauge.png"
+import timeGaugeJSON from "../assets/images/timegauge.json"
+
+
+import { TimeGauge } from "../object/timegauge";
+import { Player } from "../object/player";
 
 export default class Stage02 extends Phaser.Scene {
   constructor() {
@@ -9,6 +14,9 @@ export default class Stage02 extends Phaser.Scene {
       key: "Stage02",
     });
   }
+
+  platformLayer!: Phaser.Tilemaps.TilemapLayer | any;
+  timeGauge!: TimeGauge;
 
   mapWidth: number = 60;
   mapHeight: number = 100;
@@ -18,23 +26,26 @@ export default class Stage02 extends Phaser.Scene {
   preload(): void {
     this.load.tilemapTiledJSON("map", map);
     this.load.image("terrain", terrain);
+    this.load.atlas('timeGauge', timeGaugePNG, timeGaugeJSON);
+
   }
 
   create(): void {
-    this.cameras.main.setBounds(
-      0,
-      0,
-      this.mapWidth * this.tileWidth,
-      this.mapHeight * this.tileHeight
-    );
-    this.physics.world.setBounds(
-      0,
-      0,
-      this.mapWidth * this.tileWidth,
-      this.mapHeight * this.tileHeight
-    );
-    this.cameras.main.scrollX = 480;
-    this.cameras.main.scrollY = 1200;
+    // this.cameras.main.setBounds(
+    //   0,
+    //   0,
+    //   this.mapWidth * this.tileWidth,
+    //   this.mapHeight * this.tileHeight
+    // );
+    // this.physics.world.setBounds(
+    //   0,
+    //   0,
+    //   this.mapWidth * this.tileWidth,
+    //   this.mapHeight * this.tileHeight
+    // );
+    // this.cameras.main.scrollX = 480;
+    // this.cameras.main.scrollY = 1200;
+    
     const map = this.make.tilemap({
       key: "map",
       tileWidth: 16,
@@ -42,7 +53,9 @@ export default class Stage02 extends Phaser.Scene {
     });
 
     map.addTilesetImage("terrain", "terrain");
-    map.createLayer("platformLayer", ["terrain"]);
+    this.platformLayer = map.createLayer("platformLayer", ["terrain"]);
+    this.timeGauge = new TimeGauge(this, this.game.canvas.width/2, this.game.canvas.height/6, "timeGauge").setScrollFactor(0);
+
   }
 
   update(): void {}
