0fda8ba1979fbe4309005c6702c58a433aec6c7a
seoyoung81 <seosue0801@gmail.com>
2023-08-07 17:54:32 +0900

    feat: 아이템 구매 장착 체크

--- frontend/src/web/components/MyPage/UserItem/MyItem.tsx
+++ frontend/src/web/components/MyPage/UserItem/MyItem.tsx
@@ -5,27 +5,34 @@ import { BsCheck } from 'react-icons/bs';
 
 import styles from './UserItem.module.css';
 
-const MyItem: React.FC = () => {
-    const [check, setCheck] = useState<boolean>(false);
-    const onEquip = () => { 
-        setCheck(!check);
-    };
-    // check 가 true 이면 아이템 장착하는 API 요청 보내기
-    if (check) {
-        // 아이템 장착 로직
-    }
+interface MyItemProps {
+    itemId: string;
+    itemName: string;
+    checked: boolean;
+    onEquip: (itemId: string) => void;
+}
+
+const MyItem: React.FC<MyItemProps> = ({ itemId, itemName, checked, onEquip }) => {
+    const onEquipItemClick = () => {
+        if (checked) {
+            onEquip(""); 
+          } else {
+            onEquip(itemId);
+          }
+      };
+
     return (
         <div>
             <div 
                 className={styles['myitem-container']}
-                onClick={onEquip}
+                onClick={onEquipItemClick}
             >
                 <div>
                     <MyItemImg />
                 </div>
                 <MyItemName />
             <div className={styles['check-icon']}>
-                {check ? <BsCheck size={180} /> : null}
+                {checked ? <BsCheck size={180} /> : null}
             </div>
             </div>
         </div>
--- frontend/src/web/components/MyPage/UserItem/MyItemList.tsx
+++ frontend/src/web/components/MyPage/UserItem/MyItemList.tsx
@@ -1,16 +1,57 @@
 import MyItem from './MyItem';
+import { useState, useEffect } from 'react';
 import styles from './UserItem.module.css';
+import { authInstance } from '../../../services/api';
 
 const MyItemList: React.FC = () => {
+    const [itemList, setItemList] = useState<any[]>([]);
+    const [checkedItemId, setCheckedItemId] = useState<string | null>(null);
+
+    const handleEquipItem = (itemId: string) => {
+        setCheckedItemId(itemId);
+    };
+
+    // const itemList = [
+    //     { id: 'item1', name: 'Item 1' },
+    //     { id: 'item2', name: 'Item 2' },
+    //     { id: 'item3', name: 'Item 3' },
+    //     { id: 'item4', name: 'Item 4' },
+    //     { id: 'item5', name: 'Item 5' },
+    //     { id: 'item6', name: 'Item 6' },
+    //     { id: 'item7', name: 'Item 7' },
+    
+    //   ];
+
+   
+    useEffect(() => {
+    const fetchItemList = async () => {
+        try {
+            const { data } = await authInstance('/member/item/purchased'); 
+            setItemList(data);
+            console.log('내가 구매한 아이템 불러오기 성공', data);
+            return data;
+        }  
+        catch (error) {
+            console.log('내가 구매한 아이템 불러오기 실패', error);
+        }
+    };
+
+    fetchItemList();
+    }, []);
+    
+
     return (
         <div className={styles['myitem-wrap']}>
-            <MyItem />
-            <MyItem />
-            <MyItem />
-            <MyItem />
-            <MyItem />
-            <MyItem />
-            <MyItem />
+            {itemList.map(item => (
+            <MyItem
+                key={item.id}
+                itemId={item.id}
+                itemName={item.name}
+                checked={item.id === checkedItemId}
+                onEquip={handleEquipItem}
+            />
+      ))}
+          
         </div>
     )
 };
