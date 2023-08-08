4ae40622192c797c263e4582fbdff6c289df4f8e
박재훈 <jhp336@naver.com>
2023-08-08 13:40:02 +0900

    Fix/cors config

--- backend/src/main/java/com/ssafy/a305/global/security/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/a305/global/security/config/WebMvcConfig.java
@@ -11,6 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*") // 허용할 도메인 설정
-			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"); // 허용할 HTTP 메소드 설정
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메소드 설정
+			.allowedHeaders("*");
 	}
 }
