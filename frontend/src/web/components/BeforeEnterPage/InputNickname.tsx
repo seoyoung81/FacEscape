c10f5ccab3f4f7bb05a5c0ab5d1557762cb69093
김한성 <anjfkrhdkdle@naver.com>
2023-08-11 08:27:36 +0900

    Frontend/feat/room-visit

--- frontend/src/web/components/BeforeEnterPage/InputNickname.tsx
+++ frontend/src/web/components/BeforeEnterPage/InputNickname.tsx
@@ -1,7 +1,5 @@
-import ControlIcon from '../Common/ControlIcon';
 import styles from './BeforeEnter.module.css';
 import { useState, ChangeEvent, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setNickName } from '../../store/nickNameSlice';
 
@@ -9,7 +7,7 @@ const InputNickname: React.FC = () => {
   const [value, setValue] = useState<string>("");
   const [defaultNickName, setDefaultNickName] = useState<string | null | readonly string[]>("");
   const [showError, setShowError] = useState(false); // 에러 표시 여부를 결정하는 상태 변수
-  const navigate = useNavigate();
+
   const dispatch = useDispatch();
 
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -22,8 +20,8 @@ const InputNickname: React.FC = () => {
   const handleClick = () => {
     if (value) {
       console.log("닉네임 : " + value);
-      navigate('/waiting');
       setNickName(value);
+      window.location.href = "/waiting";
     } else {
       setShowError(true); // 에러 상태를 표시합니다.
     }
@@ -44,22 +42,19 @@ const InputNickname: React.FC = () => {
   }, [])
 
   return (
-    <div className={styles['chat-layout']}>
-      <div>
-        <ControlIcon openVidu={undefined} />
-      </div>
-      <div className={styles['input-container']}>
-        <input
-          type="text"
-          className={styles['nickname-input']}
-          value={value}
-          onChange={onChange}
-          onKeyDown={handleKeyDown}
-          placeholder={showError ? "아이디를 입력해주세요" : ""}
-        />
-        <button className={styles['enter-btn']} onClick={handleClick}>입장</button>
-      </div>
-    </div>
+    <>
+        <div className={styles['input-container']}>
+          <input
+            type="text"
+            className={styles['nickname-input']}
+            value={value}
+            onChange={onChange}
+            onKeyDown={handleKeyDown}
+            placeholder={showError ? "아이디를 입력해주세요" : ""}
+          />
+          <button className={styles['enter-btn']} onClick={handleClick}>입장</button>
+        </div>
+    </>
   )
 }
 
