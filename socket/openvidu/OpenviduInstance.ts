e534db3c8bfd6e499b5d2809d572638cdc0c87b4
김한성 <anjfkrhdkdle@naver.com>
2023-08-11 08:48:49 +0900

    Socket/fix/build-error

--- socket/openvidu/OpenviduInstance.ts
+++ socket/openvidu/OpenviduInstance.ts
@@ -1,4 +1,7 @@
 import { OpenVidu } from 'openvidu-node-client';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const openvidu = new OpenVidu(process.env.OPENVIDU_URL!, process.env.OPENVIDU_SECRET!);
 
