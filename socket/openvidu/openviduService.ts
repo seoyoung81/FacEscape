af2891ab8e8e67db224926a9490006b5977964e0
김한성 <anjfkrhdkdle@naver.com>
2023-08-11 22:45:51 +0900

    Frontend/feat/소켓 오류 수정

--- socket/openvidu/openviduService.ts
+++ socket/openvidu/openviduService.ts
@@ -1,10 +1,16 @@
 import openvidu from './OpenviduInstance';
 import { roomManager } from "../room/roomManager";
+import { memberManager } from '../member/memberManager';
 import express from "express";
 
 const opService = {
     createSession: async (req: express.Request) => {
         const requestedRoomId = req.body.customSessionId;
+        const hostIp = req.socket.remoteAddress || "";
+
+        if(!memberManager.exists(hostIp)) {
+            return { error: "존재하지 않는 사용자 입니다." };
+        } 
 
         if (!requestedRoomId){
             return { error: '올바르지 않는 roomId값입니다.' };
@@ -17,8 +23,11 @@ const opService = {
         return { sessionId: session.sessionId };
     },
 
-    createConnection: async (requestedRoomId:string, req: express.Request) => {
-        
+    createConnection: async (hostIp:string, requestedRoomId:string, req: express.Request) => {
+        if(!memberManager.exists(hostIp)) {
+            return { error: "존재하지 않는 사용자 입니다." };
+        } 
+
         if (!requestedRoomId) {
             return { error: '올바르지 않는 roomId값입니다.' };
         } else if (isNonExistRoom(requestedRoomId)) {
@@ -28,28 +37,19 @@ const opService = {
         if(isRoomFull(requestedRoomId)) {
             return { error: '이미 만원인 방에 대한 화상 서버 입장 요청입니다.' };
         }
-
-        // if(invaildateUser(requestedRoomId)) {
-        //     return { error: '방에 존재하지 않는 유저의 화상 서버 입장 요청입니다' };
-        //      나중에 조건에 따라 바꿀것
-        // }
-           
-        const session = openvidu.activeSessions.find(
-                (s) => s.sessionId === requestedRoomId
-            );
-            
-            if (!session) {
-                return { error: '초 비상 !!!!! 방은 존재하지만 화상 서버는 존재하지 않습니다' };
-            }
-            else {
-                const connection = await session.createConnection(req.body);
-                return { token: connection.token };
-            }
+    
+        const session = openvidu.activeSessions.find((s) => s.sessionId === requestedRoomId);
+        if (!session) {
+            return { error: '초 비상 !!!!! 방은 존재하지만 화상 서버는 존재하지 않습니다' };
+        } else {
+            const connection = await session.createConnection(req.body);
+            return { token: connection.token };
+        }
         
     }
 };
 
-function isNonExistRoom(roomID: string | undefined | null) {
+const isNonExistRoom = (roomID: string) => {
     if (roomID) {
         return !roomManager.exists(roomID);
     } else {
@@ -57,8 +57,8 @@ function isNonExistRoom(roomID: string | undefined | null) {
     }
 }
 
-function isRoomFull(roomID: string) {
-    return roomManager.getRoom(roomID)?.isFull;
+const isRoomFull = (roomID: string) => {
+    return roomManager.getRoom(roomID)?.isFull();
 }
 
 export default opService;
--- socket/socket/socket.model.ts
+++ socket/socket/socket.model.ts
@@ -1,7 +1,6 @@
 import { Server } from "socket.io";
 import { ConnectEvent, JoinEvent, ExitEvent, MemberActionEvent, GameActionEvent, GameResponseEvent, MemberResponseEvent } from "../socket/utils/eventType";
 import { createRoomEventHandler, joinRoomEventHandler, exitEventHandler, memberChatEventHandler, memberNickNameEventHandler, gameStartEventhandler } from "./utils/roomSocketEventHandler"
-import { RoomInfoResponse } from "./utils/roomInfoResponse";
 
 const socketMapper = (httpServer: any) => {
     const io = new Server(httpServer, {
@@ -13,9 +12,6 @@ const socketMapper = (httpServer: any) => {
     });
 
     io.on(ConnectEvent.connection, (socket) => {
-        //console.log(`Connected: ${socket.id}`);
-       // console.log(`address :  ${socket.handshake.address}`)
-      
         // 방 생성 Event Handler
         socket.on(JoinEvent.create, () => {
           console.log("방 생성 요청 감지 : createRoomEventHandle 실행");
@@ -30,17 +26,11 @@ const socketMapper = (httpServer: any) => {
         // 방 떠나기 또는 소켓 연결이 끊어진 경우(브라우저 종료 등)
         Object.values(ExitEvent).forEach(event=>{
           socket.on(event, ()=>{
-          //  console.log(`Disconnected: ${socket.id}`);
-          //  console.log(io.sockets.adapter.rooms);
+           console.log(`Disconnected: ${socket.id}`);
+           console.log(io.sockets.adapter.rooms);
             exitEventHandler(event, socket);
           })
         });
-
-        // socket.on(MemberActionEvent.chat, (msg: string)=>{
-        //   memberChatEventHandler(socket, msg, (roomId:string, chat: string) => {
-        //     return io.to(roomId).emit(MemberActionEvent.chat, chat);
-        //   });
-        // });
 
         socket.on(MemberActionEvent.updateNickName, (data: any)=>{
           memberNickNameEventHandler(socket, data, (roomId:string, response: string) => {
