b60a03d92c0f67fe12a2604b1848a89e62bbee79
김한성 <anjfkrhdkdle@naver.com>
2023-08-16 09:11:52 +0900

    feat: frontend 배포를 위한 설정 추가

--- socket/server.ts
+++ socket/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import socketMapper from "./socket/socket.model";
 import opController from './openvidu/openviduController'; // opController를 import
 import memberController from './member/controller/memberController'
+import path from "path";
 
 const app = express();
 const httpServer = createServer(app);
@@ -14,6 +15,11 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', opController);
 app.use('/member', memberController);
+
+app.use(express.static(path.join(__dirname, "./public")))
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
 
 const PORT = process.env.PORT || 3050;
 httpServer.listen(PORT, () => {
