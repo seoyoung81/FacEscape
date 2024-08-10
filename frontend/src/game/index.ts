be96fff40363481ff560d8c0b5189c26ab4a7b65
seoyoung81 <seosue0801@gmail.com>
2024-08-10 21:44:39 +0900

    🐛 add x

--- frontend/src/game/index.ts
+++ frontend/src/game/index.ts
@@ -17,7 +17,7 @@ export const createGame = (canvas: HTMLCanvasElement) => {
     physics: {
       default: "arcade",
       arcade: {
-        gravity: { y: 500 },
+        gravity: { x: 0, y: 500 },
         checkCollision: { up: true, down: true, left: true, right: true },
       },
     },
